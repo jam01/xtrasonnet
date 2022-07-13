c8fe7d961be5de7b0ad1b3c419b01a779e4a3aa9
jam01 <jam01@protonmail.com>
2022-07-13 18:50:49 -0500

    remove unused regex and crypto

--- src/main/java/com/datasonnet/modules/Crypto.java
+++ src/main/java/com/datasonnet/modules/Crypto.java
@@ -38,50 +38,6 @@ import java.util.Base64;
 
 
 public class Crypto {
-
-    public static String encrypt(String value, String keyString)
-            throws Exception {
-        BlowfishEngine engine = new BlowfishEngine();
-        PaddedBufferedBlockCipher cipher =
-                new PaddedBufferedBlockCipher(engine);
-        KeyParameter key = new KeyParameter(keyString.getBytes());
-        cipher.init(true, key);
-        byte[] in = value.getBytes();
-        byte[] out = new byte[cipher.getOutputSize(in.length)];
-        int len1 = cipher.processBytes(in, 0, in.length, out, 0);
-        try {
-            cipher.doFinal(out, len1);
-        } catch (CryptoException e) {
-            e.printStackTrace();
-            throw new Exception(e.getMessage());
-        }
-        String s = new String(Base64.getEncoder().encode(out));
-        return s;
-    }
-
-    public static String decrypt(String value, String keyString)
-            throws Exception {
-        BlowfishEngine engine = new BlowfishEngine();
-        PaddedBufferedBlockCipher cipher =
-                new PaddedBufferedBlockCipher(engine);
-        StringBuffer result = new StringBuffer();
-        KeyParameter key = new KeyParameter(keyString.getBytes());
-        cipher.init(false, key);
-        byte[] out = Base64.getDecoder().decode(value);
-        byte[] out2 = new byte[cipher.getOutputSize(out.length)];
-        int len2 = cipher.processBytes(out, 0, out.length, out2, 0);
-        cipher.doFinal(out2, len2);
-        String s2 = new String(out2);
-        for (int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-            if (c != 0) {
-                result.append(c);
-            }
-        }
-
-        return result.toString();
-    }
-
     /*
         Algorithm can be one of MD2, MD5, SHA-1, SHA-256, SHA-384, SHA-512
      */
