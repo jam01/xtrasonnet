ddcd6c3ec82d94acca40b1c599e0751b4de34981
jam01 <jam01@protonmail.com>
2022-08-12 19:19:18 -0500

    refactor json plugin tests

---
+++ src/test/java/com/datasonnet/plugins/JSONPluginTest.java
@@ -0,0 +1,66 @@
+package com.datasonnet.plugins;
+
+import com.datasonnet.Mapper;
+import com.datasonnet.document.DefaultDocument;
+import com.datasonnet.document.MediaTypes;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JSONPluginTest {
+
+    // TODO: 8/11/22 add other JSON elements to this
+    private final String foobar = """
+                { "foo": "bar" }""";
+
+    @Test
+    public void read_simple() throws JSONException {
+        var doc = new Mapper("payload")
+                .transform(new DefaultDocument<>(foobar, MediaTypes.APPLICATION_JSON));
+
+        JSONAssert.assertEquals(foobar, doc.getContent(), true);
+        assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
+
+    @Test
+    public void read_null() throws JSONException {
+        var doc = new Mapper("payload")
+                .transform(DefaultDocument.NULL_INSTANCE);
+
+        assertEquals("null", doc.getContent());
+        assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
+
+    @Test
+    public void write_simple() throws JSONException {
+        var doc = new Mapper("{ foo: 'bar' }")
+                .transform(DefaultDocument.NULL_INSTANCE);
+
+        JSONAssert.assertEquals(foobar, doc.getContent(), true);
+        assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
+
+    @Test
+    public void write_null() {
+        var doc = new Mapper("null")
+                .transform(DefaultDocument.NULL_INSTANCE);
+
+        assertEquals("null", doc.getContent());
+        assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
+
+    @Test
+    public void write_indent() {
+        var doc = new Mapper("{ foo: 'bar' }")
+                .transform(DefaultDocument.NULL_INSTANCE, Collections.emptyMap(), MediaTypes.APPLICATION_JSON.withParameter(DefaultJSONFormatPlugin.DS_PARAM_INDENT, "true"));
+        assertEquals("""
+                {
+                    "foo": "bar"
+                }""", doc.getContent());
+        assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
+}
