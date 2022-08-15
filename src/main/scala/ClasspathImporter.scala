0e1eee72854b19b1bf4b5cb49af18590784834ae
jam01 <jam01@protonmail.com>
2022-08-14 23:06:36 -0500

    add missing license

--- src/main/scala/ClasspathImporter.scala
+++ src/main/scala/ClasspathImporter.scala
@@ -1,3 +1,10 @@
+
+/*-
+ * Copyright 2022 Jose Montoya.
+ *
+ * Licensed under the Elastic License 2.0; you may not use this file except in
+ * compliance with the Elastic License 2.0.
+ */
 class ClasspathImporter {
 
 }
