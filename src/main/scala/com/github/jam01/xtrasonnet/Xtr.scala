d3c87eca0e61c3a35afaad512f1dc722c684e619
jam01 <jam01@protonmail.com>
2022-08-25 06:59:09 -0500

    implement duration.toParts

--- src/main/scala/com/github/jam01/xtrasonnet/Xtr.scala
+++ src/main/scala/com/github/jam01/xtrasonnet/Xtr.scala
@@ -919,6 +919,35 @@ object Xtr extends Library {
               .plusMinutes(out.getOrElse("minutes", Val.Num(pos, 0)).cast[Val.Num].value.toLong)
               .plusSeconds(out.getOrElse("seconds", Val.Num(pos, 0)).cast[Val.Num].value.toLong)
               .toString.substring(1)
+      },
+
+      builtin("toParts", "str") {
+        (pos, ev, duration: String) =>
+          val out = new util.LinkedHashMap[String, Val.Obj.Member]
+          val timeIdx = duration.indexOf('T')
+          var period: Period = null
+          var dduration: Duration = null
+
+          if (timeIdx != -1) {
+            dduration = Duration.parse('P' + duration.substring(timeIdx))
+            period = Period.parse(duration.substring(0, timeIdx))
+          } else {
+            period = Period.parse(duration.substring(0, timeIdx))
+          }
+
+          out.put("years", memberOf(Val.Num(pos, period.getYears)))
+          out.put("months", memberOf(Val.Num(pos, period.getMonths)))
+          out.put("days", memberOf(Val.Num(pos, period.getDays)))
+          if (dduration != null) { // TODO: probably super inefficient
+            val hours = dduration.toHours
+            val minutes = dduration.minusHours(hours).toMinutes
+            val seconds = dduration.minusHours(hours).minusMinutes(minutes).toSeconds
+            out.put("hours", memberOf(Val.Num(pos, hours)))
+            out.put("minutes", memberOf(Val.Num(pos, minutes)))
+            out.put("seconds", memberOf(Val.Num(pos, seconds)))
+          }
+
+          new Val.Obj(pos, out, false, null, null)
       }
     ),
 
--- src/test/java/com/github/jam01/xtrasonnet/modules/DurationTest.java
+++ src/test/java/com/github/jam01/xtrasonnet/modules/DurationTest.java
@@ -25,7 +25,6 @@ public class DurationTest {
                 xtr.duration.of(parts)"""));
     }
 
-    @Disabled
     @Test
     public void toParts() {
         assertEquals(transform("""
@@ -34,86 +33,4 @@ public class DurationTest {
                     hours: 12, minutes: 30, seconds: 45
                 }"""), transform("xtr.duration.toParts('P20Y3M1DT12H30M45S')"));
     }
-//
-//    @Test
-//    public void nother1() {
-//        OffsetDateTime date1 = OffsetDateTime.parse("2039-12-22T07:24:26Z");
-//        OffsetDateTime date2 = OffsetDateTime.parse("2019-09-20T18:53:41Z");
-//        var dur = Duration.between(date1.toLocalTime(), date2.toLocalTime());
-//        date2 = date2.plus(dur);
-//
-//        var per = Period.between(date1.toLocalDate(), date2.toLocalDate());
-//        var isNeg = per.isNegative();
-//        per = isNeg ? per.negated() : per;
-//
-//        System.out.println(per);
-//
-////        var revDur = Duration.between(date2.toLocalTime(), date1.toLocalTime());
-////        System.out.println(per + dur.abs().toString().substring(1));
-//    }
-//
-//    @Test
-//    public void nother() {
-//        OffsetDateTime date1 = OffsetDateTime.parse("2039-12-22T07:24:26.425Z");
-//        OffsetDateTime date2 = OffsetDateTime.parse("2019-09-20T18:53:41.425Z");
-//        var dur = Duration.between(date1, date2);
-//        var isNeg = dur.isNegative();
-//        var durStr = dur.abs().toString();
-//        System.out.println(durStr);
-//        var hoursIdx = durStr.indexOf('H');
-//        if (hoursIdx != -1) {
-//            var hours = Long.valueOf(durStr.substring(2, hoursIdx));
-//            System.out.println(hours);
-//            var days = (int) (hours / 24);
-//            System.out.println(days);
-//            hours = hours % 24;
-//            System.out.println(hours);
-//
-//            var per = Period.between(date1.toLocalDate(), date1.toLocalDate().plusDays(days)).toString();
-//            System.out.println(per);
-//            System.out.println((isNeg ? "-" : "") + per + "T" + hours + durStr.substring(hoursIdx));
-//        }
-//    }
-//
-//    @Test
-//    public void nother2() {
-//        var dur = Duration.between(OffsetDateTime.parse("2039-12-22T07:24:26.425Z"), OffsetDateTime.parse("2019-09-20T18:53:41.425Z"));
-//        var isNeg = dur.isNegative();
-//        var durStr = dur.abs().toString();
-//        System.out.println(durStr);
-//        var hoursIdx = durStr.indexOf('H');
-//        if (hoursIdx != -1) {
-//            var hours = Long.valueOf(durStr.substring(2, hoursIdx));
-//            System.out.println(hours);
-//            var days = (int) (hours / 24);
-//            System.out.println(days);
-//            hours = hours % 24;
-//            System.out.println(hours);
-//
-//            var per = Period.ofDays(days).toString();
-//            System.out.println(per);
-//            System.out.println((isNeg ? "-" : "") + per + "T" + hours + durStr.substring(hoursIdx));
-//        }
-//    }
-//
-//    @Test
-//    public void main() {
-//        System.out.println(
-//                transform("""
-//                        xtr.duration.of({
-//                            years: 20, months: 3, days: 1,
-//                            hours: 12, minutes: 30, seconds: 45
-//                        })""")
-//        );
-//
-//        System.out.println(OffsetDateTime.parse("2019-09-20T18:53:41.425Z").plus(Period.parse("P20Y3M1D")).plus(Duration.parse("PT12H30M45S")));
-//
-////        System.out.println(
-////                transform("xtr.duration.toParts('P20Y3M1DT12H30M45S')")
-////        );
-//
-//        System.out.println(
-//                transform("xtr.duration.between('2039-12-22T07:24:26.425Z', '2019-09-20T18:53:41.425Z')")
-//        );
-//    }
 }
