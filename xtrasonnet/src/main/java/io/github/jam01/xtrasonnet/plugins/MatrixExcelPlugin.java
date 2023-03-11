38b9dd0176aba7042fa201a2c82776c7c9fb8f2e
jam01 <jam01@protonmail.com>
2023-03-11 12:52:27 -0600

    xlsx empty cells test

---
+++ xtrasonnet/src/main/java/io/github/jam01/xtrasonnet/plugins/MatrixExcelPlugin.java
@@ -0,0 +1,95 @@
+package io.github.jam01.xtrasonnet.plugins;
+
+/*-
+ * Copyright 2022-2023 Jose Montoya.
+ *
+ * Licensed under the Elastic License 2.0; you may not use this file except in
+ * compliance with the Elastic License 2.0.
+ */
+
+import io.github.jam01.xtrasonnet.document.Document;
+import io.github.jam01.xtrasonnet.document.MediaType;
+import io.github.jam01.xtrasonnet.document.MediaTypes;
+import io.github.jam01.xtrasonnet.spi.BasePlugin;
+import io.github.jam01.xtrasonnet.spi.PluginException;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import sjsonnet.EvalScope;
+import sjsonnet.Position;
+import sjsonnet.Val;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MatrixExcelPlugin extends BasePlugin {
+    public MatrixExcelPlugin() {
+        supportedTypes.add(MediaTypes.APPLICATION_EXCEL);
+
+        readerSupportedClasses.add(InputStream.class);
+        readerSupportedClasses.add(File.class);
+    }
+
+    @Override
+    public boolean canWrite(MediaType requestedType, Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public Val.Literal read(Document<?> doc, Position pos) throws PluginException {
+        if (doc.getContent() == null) {
+            return new Val.Null(new Position(null, 0));
+        }
+
+        var bVisitor = new LiteralVisitor(pos).visitArray(-1, -1).narrow();
+        Class<?> targetType = doc.getContent().getClass();
+
+        try (Workbook book = File.class.isAssignableFrom(targetType) ? WorkbookFactory.create((File) doc.getContent()) : WorkbookFactory.create((InputStream) doc.getContent())) {
+            var sheets = book.iterator();
+
+            while (sheets.hasNext()) {
+                var sheet = sheets.next();
+                var rows = sheet.iterator();
+                var sVisitor = bVisitor.subVisitor().visitArray(-1, -1).narrow();
+
+                while (rows.hasNext()) {
+                    var row = rows.next();
+                    var cells = row.iterator();
+                    var rVisitor = sVisitor.subVisitor().visitArray(-1, -1).narrow();
+
+                    while (cells.hasNext()) {
+                        var cell = cells.next();
+                        var cVisitor = rVisitor.subVisitor();
+
+                        var type = cell.getCellType();
+                        Val.Literal val = null;
+                        if (CellType.BOOLEAN == type) {
+                            if (cell.getBooleanCellValue()) val = (Val.Literal) cVisitor.visitTrue(-1);
+                            else cVisitor.visitFalse(-1);
+                        } else if (CellType.NUMERIC == type) {
+                            val = (Val.Literal) cVisitor.visitFloat64(cell.getNumericCellValue(), -1);
+                        } else if (CellType.STRING == type || CellType.FORMULA == type || CellType.BLANK == type) {
+                            val = (Val.Literal) cVisitor.visitString(cell.getStringCellValue(), -1);
+                        } else {
+                            throw new IllegalArgumentException("Cannot represent type: " + type.toString() + " as a jsonnet element");
+                        }
+
+                        rVisitor.visitValue(val, -1);
+                    }
+                    sVisitor.visitValue(rVisitor.visitEnd(-1), -1);
+                }
+                bVisitor.visitValue(sVisitor.visitEnd(-1), -1);
+            }
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+
+        return bVisitor.visitEnd(-1);
+    }
+
+    @Override
+    public <T> Document<T> write(Val input, MediaType mediaType, Class<T> targetType, EvalScope ev) throws PluginException {
+        throw new UnsupportedOperationException("Writing excel files is unsupported");
+    }
+}
--- xtrasonnet/src/test/java/io/github/jam01/xtrasonnet/plugins/ExcelPluginTest.java
+++ xtrasonnet/src/test/java/io/github/jam01/xtrasonnet/plugins/ExcelPluginTest.java
@@ -59,4 +59,36 @@ public class ExcelPluginTest {
         JSONAssert.assertEquals(simple_xlsx_json, doc.getContent(), true);
         Assertions.assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
     }
+
+    public final String simple_empty_cells = """
+            {
+                "Sheet1":[
+                    {"B":"b2","C":"c2","D":"d2","E":"e2"},
+                    {"B":"b3","C":"c3","D":"d3","E":"e3"},
+                    {"B":"b4","C":"c4","D":"d4","E":"e4"},
+                    {"B":"b5","C":"c5","D":"d5","E":"e5"}
+                ],
+                "Sheet2":[
+                    {"A":"a1","C":"c1","D":"d1","E":"e1"},
+                    {"A":"a3","C":"c3","D":"d3","E":"e3"},
+                    {"A":"a4","C":"c4","D":"d4","E":"e4"},
+                    {"A":"a5","C":"c5","D":"d5","E":"e5"}
+                ],
+                "Sheet3":[
+                        {"A":"a1","B":"b1","D":"d1","E":"e1"},
+                        {"A":"a2","B":"b2","D":"d2","E":"e2"},
+                        {"A":"a4","B":"b4","D":"d4","E":"e4"},
+                        {"A":"a5","B":"b5","D":"d5","E":"e5"}
+                ]
+            }
+            """;
+
+    @Test
+    public void read_xlsx_simple_empty_cells() throws JSONException {
+        var doc = new Transformer("payload")
+                .transform(Document.of(TestUtils.resourceAsFile("simple-empty-cells.xlsx"), MediaTypes.APPLICATION_EXCEL));
+
+        JSONAssert.assertEquals(simple_empty_cells, doc.getContent(), true);
+        Assertions.assertEquals(MediaTypes.APPLICATION_JSON, doc.getMediaType());
+    }
 }
